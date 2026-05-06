f9f88eba25d77e65517772584cd049668347542f
Breno <75366737+Kazees@users.noreply.github.com>
2026-05-06 15:52:08 -0300

    [Configuração] Configurando Database, DataSource e as Migrations
    feat: configure database and datasource
    feat: configure migrations scripts

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DatabaseConfig } from './configs/database/database.config';
+
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ 
+      isGlobal: true 
+    }),
+    TypeOrmModule.forRootAsync({
+      useClass: DatabaseConfig,
+      inject: [DatabaseConfig],
+    }),
+    
+    ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ConfigService],
 })
 export class AppModule {}
